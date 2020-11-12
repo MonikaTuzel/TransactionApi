319ed5a8b935079c4c3e25dbd41a6da074eda612
MonikaTuzel <61913357+MonikaTuzel@users.noreply.github.com>
2020-11-12 18:41:00 +0100

    Add files via upload

---
+++ Models/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransactionsApi.Models
+{
+    public class Transaction
+    {
+        public int idTransactions { get; set; }
+        public string User { get; set; }
+        public int Car { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public float Price { get; set; }
+        public bool IsEnd { get; set; }
+        public bool IsReturned { get; set; }
+    }
+}
